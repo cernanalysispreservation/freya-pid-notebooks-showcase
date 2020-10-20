996601417350b14ed99f0ddccce9120762e9ff99
Pamfilos Fokianos <pamfilosf@gmail.com>
2020-10-21 01:00:54 +0200

    small updates and fixes
    Signed-off-by: Pamfilos Fokianos <pamfilosf@gmail.com>

--- src/templates/notebook.js
+++ src/templates/notebook.js
@@ -5,13 +5,13 @@ import { PageProps, Link } from "gatsby"
 import Layout from "../components/layout"
 import { useStaticQuery, graphql } from "gatsby"
 import { Tabs, Tab, Badge, Breadcrumb } from 'react-bootstrap';
+import useIframeContentHeight from "react-use-iframe-content-height";
 
 import NotebookPreview from "@nteract/notebook-preview";
 
 const NotebookPage = ({ data }) => {
-  let { html, internal: { content = null } = {},  codemeta = null, name=""  } = data.pidNotebook;
-
-
+  let { html, internal: { content = null } = {}, codemeta = null, name = "" } = data.pidNotebook;
+  const [iframeRef, iframeHeight] = useIframeContentHeight();
 
   let doi_id = codemeta && codemeta.identifier ? codemeta.identifier.replace("https://doi.org/", "") : ""
   let doi_link = `https://doi.org/${doi_id}`;
@@ -19,16 +19,16 @@ const NotebookPage = ({ data }) => {
     <Layout>
       <Breadcrumb>
         <Breadcrumb.Item><Link to="/">Home</Link></Breadcrumb.Item>
-        <Breadcrumb.Item active>{codemeta.name || "Notebook Preview" }</Breadcrumb.Item>
+        <Breadcrumb.Item active>{codemeta.name || "Notebook Preview"}</Breadcrumb.Item>
       </Breadcrumb>
       <hr />
       <h2>{codemeta.name}
-        {/* {
+        {
           codemeta.binderUrl &&
           <a target="_blank" href={codemeta.binderUrl} style={{ float: "right" }}>
             <img src="https://camo.githubusercontent.com/483bae47a175c24dfbfc57390edd8b6982ac5fb3/68747470733a2f2f6d7962696e6465722e6f72672f62616467655f6c6f676f2e737667" alt="Binder" data-canonical-src="https://mybinder.org/badge_logo.svg" style={{ maxWidth: "100%" }}></img>
           </a>
-        } */}
+        }
       </h2>
       {
         doi_link !== "https://doi.org/" &&
@@ -43,8 +43,8 @@ const NotebookPage = ({ data }) => {
       <h2 />
       <br /><hr />
       <div>
-          <iframe src={name} width="1000" height="1000" title="aa"/>
-        </div>
+        <iframe ref={iframeRef} frameBorder="0" height={iframeHeight+200} width="100%" src={name} />
+      </div>
     </Layout>
   )
 }
@@ -52,7 +52,7 @@ const NotebookPage = ({ data }) => {
 
 
 
-export const query =  graphql`
+export const query = graphql`
   query($slug: String!) {
     pidNotebook(slug: { eq: $slug } ) {
         name
@@ -60,6 +60,7 @@ export const query =  graphql`
             identifier
             codeRepository
             controlledTem
+            binderUrl
             datePublished
             dateModified
             dateCreated
