8219f731190a0ed6cad181c16ccc02cb3b986ad5
Pamfilos Fokianos <pamfilosf@gmail.com>
2020-10-21 03:07:15 +0200

    updates
    Signed-off-by: Pamfilos Fokianos <pamfilosf@gmail.com>

--- README.md
+++
@@ -1,6 +0,0 @@
-FREYA PID Notebooks website
-======
-
-This is the code repository behind the PID Jupyter Notebooks showcase https://pidnotebooks.org
-
-It has been developed for the [FREYA Project](https://www.project-freya.eu/)
---
+++ component---src-pages-404-js-b0dc43fdf56cf32028da.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{w2l6:function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),l=t.n(a);t("Bl7J"),t("vrFN");e.default=function(){return l.a.createElement("span",null,l.a.createElement("span",null,"NOT FOUND"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b0dc43fdf56cf32028da.js.map
